ba04ba2051220743ff88759745cd50e273065b49
Adauto Turíbio <adautoturibio73@gmail.com>
2024-08-01 14:05:55 -0300

    ajustes no navbar

--- app/Views/Templates/nav_user.php
+++ app/Views/Templates/nav_user.php
@@ -4,8 +4,8 @@
         <img src="/assets/imagens/logo.png" alt="Logo" class="navbar-logo">
       </a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="#"><i class="tiny material-icons left">restaurant_menu</i>Cardápio</a></li>
-        <li><a href="#"><i class="tiny material-icons left">local_mall</i>Sacola</a></li>
+        <li><a href="#" class="hover-yellow"><i class="tiny material-icons left">restaurant_menu</i>Cardápio</a></li>
+        <li><a href="#" class="hover-yellow"><i class="tiny material-icons left">local_mall</i>Sacola</a></li>
       </ul>
     </div>
   </nav>
