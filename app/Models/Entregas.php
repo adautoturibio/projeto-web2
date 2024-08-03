0bb202a114973bf406596ba5ebec260912f55f70
Gabriel-Victor03 <gabrielvictorman@gmail.com>
2024-08-03 19:41:59 -0300

    Corrigindo arquivos models

---
+++ app/Models/Entregas.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Entregas extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'entregas';
+    protected $primaryKey       = 'entregas_id';
+    protected $useAutoIncrement = true;
+    // protected $returnType       = 'array';
+    protected $returnType       = 'object';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['tipo, endereco_entrega, pedidos_pedidos_id'];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // // Validation
+    // protected $validationRules      = [];
+    // protected $validationMessages   = [];
+    // protected $skipValidation       = false;
+    // protected $cleanValidationRules = true;
+
+    // // Callbacks
+    // protected $allowCallbacks = true;
+    // protected $beforeInsert   = [];
+    // protected $afterInsert    = [];
+    // protected $beforeUpdate   = [];
+    // protected $afterUpdate    = [];
+    // protected $beforeFind     = [];
+    // protected $afterFind      = [];
+    // protected $beforeDelete   = [];
+    // protected $afterDelete    = [];
+}
--- app/Models/UsuarioModel.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    namespace App\Models;
-    use CodeIgniter\Model;
-    class UsuarioModel extends Model{
-        protected $table = 'usuario';
-    }
-?>
