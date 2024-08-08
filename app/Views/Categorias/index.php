83c784934907b02d0db44163473f596001fea5bc
Adauto Turíbio <adautoturibio73@gmail.com>
2024-08-08 04:18:31 -0300

    personalinação adm categorias

--- app/Views/Categorias/index.php
+++ app/Views/Categorias/index.php
@@ -12,46 +12,49 @@
 
     <div class="container">
 
-        <h2 class="border-bottom border-2 border-primary mt-3 mb-4"> <?= $title ?> </h2>
+        <h3 class="cor-1t"><b> <?= $title ?></b></h3>
+        <hr class="divider">
 
         <?php if(isset($msg)){echo $msg;} ?>
 
-        <form action="<?= base_url('categorias/search'); ?>" class="d-flex" role="search" method="post">
-            <input class="form-control me-2" name="pesquisar" type="search"
-                placeholder="Pesquisar" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">
-                <i class="bi bi-search"></i>
-            </button>
+        <form action="<?= base_url('categorias/search'); ?>" class="input-field" role="search" method="post">
+            <div class="input-field">
+                <input id="search" name="pesquisar" type="search" class="validate" placeholder="Pesquisar">
+                <label class="label-icon" for="search"><i class="material-icons right">search</i></label>
+                <button class="btn waves-effect waves-light green" type="submit">
+                    <i class="material-icons">search</i>
+                </button>
+            </div>
         </form>
 
-        <table class="table">
+        <table class="highlight">
             <thead>
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Categoria</th>
-                    <th scope="col">
-                        <a class="btn btn-success"  href="<?= base_url('categorias/new'); ?>">
+                    <th>ID</th>
+                    <th>Categoria</th>
+                    <th>
+                        <a class="btn waves-effect waves-light green" href="<?= base_url('categorias/new'); ?>">
                             Novo
-                            <i class="bi bi-plus-circle"></i>
+                            <i class="material-icons">add_circle</i>
                         </a>
                     </th>
                 </tr>
             </thead>
-            <tbody class="table-group-divider">
+            <tbody>
                 
                 <!-- Aqui vai o laço de repetição -->
                 <?php for($i=0; $i < count($categorias); $i++){ ?>
                     <tr>
-                        <th scope="row"><?= $categorias[$i]->categorias_id; ?></th>
+                        <td><?= $categorias[$i]->categorias_id; ?></td>
                         <td><?= $categorias[$i]->nome; ?></td>
                         <td>
-                            <a class="btn btn-primary"  href="<?= base_url('categorias/edit/'.$categorias[$i]->categorias_id); ?>">
+                            <a class="btn waves-effect waves-light blue" href="<?= base_url('categorias/edit/'.$categorias[$i]->categorias_id); ?>">
                                 Editar
-                                <i class="bi bi-pencil-square"></i>
+                                <i class="material-icons">edit</i>
                             </a>
-                            <a class="btn btn-danger"  href="<?= base_url('categorias/delete/'.$categorias[$i]->categorias_id); ?>">
+                            <a class="btn waves-effect waves-light red" href="<?= base_url('categorias/delete/'.$categorias[$i]->categorias_id); ?>">
                                 Excluir
-                                <i class="bi bi-x-circle"></i>
+                                <i class="material-icons">delete</i>
                             </a>
                         </td>
                     </tr>
@@ -61,6 +64,7 @@
         </table>
 
     </div>
+<br><br><br><br>
 <?= $this->endSection() ?>
 
 <?php 
