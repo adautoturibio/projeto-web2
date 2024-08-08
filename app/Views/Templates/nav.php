241d4f387ec506297a67bf4403b6e8bea0563f3b
Adauto Turíbio <adautoturibio73@gmail.com>
2024-08-08 02:06:19 -0300

    arrumando links do navbar

--- app/Views/Templates/nav.php
+++ app/Views/Templates/nav.php
@@ -2,19 +2,19 @@
 <nav>
     <div class="nav-wrapper cor-1b">
         <!-- LOGO -->
-        <a class="brand-logo" href="#"><img src="/assets/imagens/logo.png" alt="Logo" class="navbar-logo"></a>
+        <a class="brand-logo" href="/home"><img src="/assets/imagens/logo.png" alt="Logo" class="navbar-logo"></a>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="#" class="hover-yellow" id="menu-cardapio"><i class="tiny material-icons left">restaurant_menu</i>Menu</a></li>
-            <li><a href="#" class="hover-yellow" id="sacola-menu"><i class="tiny material-icons left">local_mall</i>Sacola</a></li>
+            <li><a href="/home" class="hover-yellow" id="menu-cardapio"><i class="tiny material-icons left">restaurant_menu</i>Menu</a></li>
+            <li><a href="/sacola" class="hover-yellow" id="sacola-menu"><i class="tiny material-icons left">local_mall</i>Sacola</a></li>
         </ul>
     </div>
 </nav>
 
 <!-- Menu Lateral -->
 <ul class="sidenav" id="mobile-demo">
-    <li><a href="#hamburguer" class="hover-yellow" id="menu-cardapio"><i class="tiny material-icons left">restaurant_menu</i>Menu</a></li>
-    <li><a href=""><i class="tiny material-icons left">local_mall</i>Sacola</a></li>
+    <li><a href="/home" class="hover-yellow" id="menu-cardapio"><i class="tiny material-icons left">restaurant_menu</i>Menu</a></li>
+    <li><a href="/sacola"><i class="tiny material-icons left">local_mall</i>Sacola</a></li>
 </ul>
 
 <script>
